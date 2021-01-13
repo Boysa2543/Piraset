e6dabbefb727d952bc55bb1fc8ae427b19fc69bb
Boysa2543 <6206021622135u@fitm.kmutnb.ac.th>
2021-01-13 09:10:51 +0700

    create index.js

---
+++ datatype.js
@@ -0,0 +1,16 @@
+const people=["Aaron","Mel","John"];
+const one = 1;
+const str ="Hello World";
+const b = true;
+const employee={
+    firstName :"Piraset",
+    LastName :"Janjit",
+};
+
+funtion sayHello{person}{
+    console.log{"hello"+person.firstName};
+}
+console.log{typeof people};
+console.log{typeof sayHello};
+console.log{employee instanceof Array};
+sayHello{employee};
